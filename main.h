6b722a4f333ca49d4ebc10272f816ad36c4876c6
Oluwatiseunla <abduloluwaseun16@gmail.com>
2022-11-22 05:43:10 -0800

    printf project

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -22,7 +22,7 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - struct 
+ * struct fmt - struct
  * @fmt: format
  * @fn: associated func
  */
@@ -44,33 +44,33 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-va_list, char buffer[], int flags, int width, int precision, int size);
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
 /* Functions to print characters(char) and strings */
 
 int print_char(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
 /* Functions to print nums */
 
 int print_int(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_octal(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, int width, int precision, int size);
@@ -78,12 +78,12 @@ char buffer[], int flags, int width, int precision, int size);
 /* Function to print non printable characters */
 
 int print_non_printable(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
 /* Function to print memory address */
 
 int print_pointer(va_list types, char buffer[],
-                int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
 /* Function to handle other specifics */
 
@@ -95,23 +95,23 @@ int get_size(const char *format, int *i);
 /* Function to print string in reverse */
 
 int print_reverse(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
-/* Function to print a string in rot 13 */
+/* Function to print a string in rot 13*/
 
 int print_rot13string(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 
 /* width handle */
 
 int handle_write_char(char c, char buffer[],
-		int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
-		int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width,int precision,
-		int length, char padd, char extra_c);
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
-		int width, int flags, char padd, char extra_c, int padd_start);
+	int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
