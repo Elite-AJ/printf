6da2a4a0fc74fbf195697ddd3272b32e54e8fd8f
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-23 05:14:50 +0100

    gcc

--- functions1.c
+++ functions1.c
@@ -91,8 +91,8 @@ int print_octal(va_list types, char buffer[],
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	return (print_hexa(types, "0123456789abcdef", buffer,
-		flags, 'x', width, precision, size));
+	return (print_hexa(types, "0123456789abcdef", buffer, 
+				flags, 'x', width, precision, size));
 }
 
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
